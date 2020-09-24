19a32821b9afa2e292996f2352f6e0a616e7cb9c
arielsaucedo <arielsaucedo@gmail.com>
2020-09-23 21:16:50 -0300

    Se crea register.html y login.html - Se genera formularios y ruteo

--- app.js
+++ app.js
@@ -5,18 +5,29 @@ const path = require("path")
 
 /* Ruta raiz */
 app.get("/", function(req, res) {
-    let file = path.resolve('scr/index.html')    
-    res.sendFile(file)
+    let rutafile = path.resolve('scr/index.html')    
+    res.sendFile(rutafile)
 })
 
-    app.get('*', function(req, res){
+/* Ruta register */
+app.get("/register", function(req, res) {
+    let rutafile = path.resolve('scr/register.html')    
+    res.sendFile(rutafile)
+})
 
-        // resuelve ruta de estilos e imagenes
-            if(req.url.includes('.')) {
-                let file = path.resolve('public' + req.url)
-                return res.sendFile(file)
-            }
-        res.send('Not found')
+/* Ruta login */
+app.get("/login", function(req, res) {
+    let rutafile = path.resolve('scr/login.html')    
+    res.sendFile(rutafile)
+})
+
+/* resuelve ruta de estilos e imagenes */
+app.get('*', function(req, res){
+    if(req.url.includes('.')) {
+    let file = path.resolve('public' + req.url)
+    return res.sendFile(file)
+    }
+    res.send('Not found')
 })
 /* Llamado a servidor - Puerto 3000 */
 app.listen(3000)
